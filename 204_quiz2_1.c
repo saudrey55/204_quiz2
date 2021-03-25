96947b7e1150776a543967f4feb7e7b908f096a0
saudrey <weerawutearth@gmail.com>
2021-03-25 17:06:48 +0700

    Cube Free Numbers

---
+++ 204_quiz2_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,i,j;
+
+    scanf("%d",&a);
+
+    if (a==1)
+    {
+        printf("1");
+    }
+    else if (a == 2)
+    {
+        printf("2");
+    }
+    
+    
+    
+}
