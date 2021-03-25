91f78f26b2a0972c081701428d6319a9fae8201f
saudrey <weerawutearth@gmail.com>
2021-03-25 19:56:35 +0700

    c

---
+++ 204_quiz2_3.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    int n,x,j,d[10],z,c,i,y=0;
+    cin>>n;
+    x=n;
+
+    for ( i = 0; n==1||n==0;i++ )
+    {
+        if (n/10)
+        {
+            y++;
+        }
+       
+    }
+    for ( j = 0; j <y; j++)
+    {
+    d[j]=x%10;
+    x=x/10;
+    }
+    int z,b=0;
+     for ( j = 0; j <10; j++)
+    {
+    if (d[j]==n)
+     {
+        b++;
+        printf("%d\n",b);
+     }
+    else
+    {
+        continue;
+    }
+    }
+    
+
+    printf("%d",b);
+    
+}
