d0289b07c91d41118eaf3c6d9de33cb6a86ec832
saudrey <weerawutearth@gmail.com>
2021-03-25 19:18:11 +0700

    c

---
+++ 204_quiz2_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int m,n,i,j;
+    scanf("%d",&m);
+    scanf("%d",&n);
+
+    int b[m][n];
+
+    for ( i = 0; i < m+1; i++)
+    {
+        for ( j = 0; j < n+1; j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+        
+    }
+    
+
+
+}
